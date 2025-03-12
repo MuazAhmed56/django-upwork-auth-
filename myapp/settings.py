dbc77ad800c78238e77ecbcfe11dd33ecf517de9
MuazAhmed56 <fauzanchaudhry92@gmail.com>
2025-03-13 00:56:19 +0500

    Fixed DATABASES configuration

--- myapp/settings.py
+++ myapp/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = "myapp.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3"
+     "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3"
         
     }
 }
