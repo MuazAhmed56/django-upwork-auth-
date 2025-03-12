a721c29281497dfdbfd508e256b9b5a9e2ff9712
MuazAhmed56 <fauzanchaudhry92@gmail.com>
2025-03-12 23:22:23 +0500

    Configured PostgreSQL database

--- myapp/settings.py
+++ myapp/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = "myapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3"
+        
     }
 }
 
